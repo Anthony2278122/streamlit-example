2696f485716ac075d0dfa014b6c03f84ea543ff0
Anthony2278122 <131009385+Anthony2278122@users.noreply.github.com>
2023-12-30 20:08:50 +0100

    new test

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,27 +32,37 @@ floor_style = st.radio(
 num_baguette = st.slider("Number of points in spiral", 1, 10, 3)
 
 if starter_type == "dry yeast":
-    floor_per_baguette = 173
+    floor_per_baguette = 173.33
     starter_per_baguette = 7
     wheight = 0
+    salt_per_baguette = 3
 else:
     floor_per_baguette = 200
     starter_per_baguette = 50
     wheight = 0.5
+    salt_per_baguette = 4
 
 if floor_style == "white baguette":
     hydration = 0.72
+    total_floor = np.round(num_baguette * floor_per_baguette - wheight * num_baguette * starter_per_baguette)
+    floor_ratio = 1
 else:
     hydration = 0.72
+    total_floor = np.round(num_baguette * floor_per_baguette - wheight * num_baguette * starter_per_baguette)
+    floor_ratio = 0.8
+
 
 receipe = {
     starter_type + " [g]" : np.round(num_baguette * starter_per_baguette),
-    "total floor [g]" : np.round(num_baguette * floor_per_baguette - wheight * num_baguette * starter_per_baguette),
+    "total white floor [g]" : np.round(total_floor * floor_ratio),
+    "total salt [g]": np.round(num_baguette * salt_per_baguette),
     "total water [g]" : np.round(num_baguette * floor_per_baguette * hydration - wheight * num_baguette * starter_per_baguette)
     }
 
+receipe["whole wheat floor"] = total_floor * (1 - floor_ratio)
+
 st.subheader("needed ingredients:")
 st.write(receipe)
-st.dataframe(pd.DataFrame.from_dict(receipe, columns=["a","b","c"]))
+st.dataframe(pd.DataFrame.from_dict(receipe, orient="columns", dtype=int )
 
 st.write("Job Done!")
