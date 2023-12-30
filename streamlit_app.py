577a85a89a178f5aee4d3c96272ab242ea86fdc2
Anthony2278122 <131009385+Anthony2278122@users.noreply.github.com>
2023-12-30 20:43:49 +0100

    fix11

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if floor_style == "whole wheat baguette":
 st.subheader("needed ingredients:")
 #st.write(receipe)
 df = pd.DataFrame.from_records([receipe]).transpose()
-st.write(df.columns)
+st.write(df.columns.values)
 
 
 df.columns.values[0] = "quantities"
